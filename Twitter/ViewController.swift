41bed77dab900f669778e99de7e80f9b5f1274df
markdd1133 <shengchichen@Shengde-MacBook-Air.local>
2017-05-15 21:03:41 +0800

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Sheng Chi Chen on 2017/5/15.
+//  Copyright © 2017年 Sheng Chi Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
